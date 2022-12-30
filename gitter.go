73cd2abf2f555de0c7c7e4c3be3db79b2622d077
Natalie Fioretti <natalie.fioretti@postfinance.ch>
2022-12-30 10:05:30 +0100

    implemented CommitNewFile

--- gitter.go
+++ gitter.go
@@ -1,18 +1,16 @@
 package gitter
 
 import (
+	"github.com/go-git/go-billy/v5/util"
 	"github.com/go-git/go-git/v5/plumbing/object"
 )
 
 // Get a new gitter client
 func New(url, token string, cs *object.Signature) (*client, error) {
-	return &client{
-		url:             url,
-		token:           token,
-		commitsignature: cs,
-	}, nil
+	return initClient(url, token, cs)
 }
 
+// Helper function to create correctly structured Signatures
 func CreateCommitSignature(name, email string) *object.Signature {
 	return &object.Signature{
 		Name:  name,
@@ -20,7 +18,31 @@ func CreateCommitSignature(name, email string) *object.Signature {
 	}
 }
 
-//
-func (c *client) CommitNewFile(filename string, content []byte) error {
+// Add, commit and push new file to repository
+func (c *client) CommitNewFile(filename, message string, content []byte) error {
+	// write the file, and return if got an error
+	err := util.WriteFile(*c.fs, filename, content, 0440)
+	if err != nil {
+		return err
+	}
+
+	// add file to worktree
+	_, err = c.worktree.Add(filename)
+	if err != nil {
+		return err
+	}
+
+	// commit stuff
+	_, err = c.commit(message)
+	if err != nil {
+		return err
+	}
+
+	// push stuff
+	err = c.push()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
--- internals.go
+++ internals.go
@@ -2,35 +2,78 @@ package gitter
 
 import (
 	"os"
+	"time"
 
 	git "github.com/go-git/go-git/v5"
+	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/go-git/go-git/v5/plumbing/transport/http"
 	"github.com/go-git/go-git/v5/storage/memory"
 
 	billy "github.com/go-git/go-billy/v5"
 	"github.com/go-git/go-billy/v5/memfs"
-	"github.com/go-git/go-billy/v5/util"
 )
 
 // Client represents a Gitter Client
 type client struct {
 	url             string
 	token           string
-	commitsignature *object.Signature
+	commitSignature *object.Signature
 	storer          *memory.Storage
 	fs              *billy.Filesystem
+	repo            *git.Repository
+	worktree        *git.Worktree
 }
 
-func initClient(url, token string, cs *object.Signature) *client {
+func initClient(url, token string, cs *object.Signature) (*client, error) {
 	s := memory.NewStorage()
 	fs := memfs.New()
+
+	r, err := git.Clone(s, fs, cloneOptions(url, token))
+	if err != nil {
+		return nil, err
+	}
+
+	w, err := r.Worktree()
+	if err != nil {
+		return nil, err
+	}
+
 	return &client{
 		url:             url,
 		token:           token,
-		commitsignature: cs,
+		commitSignature: cs,
 		storer:          s,
 		fs:              &fs,
+		repo:            r,
+		worktree:        w,
+	}, nil
+}
+
+func cloneOptions(url, token string) *git.CloneOptions {
+	return &git.CloneOptions{
+		URL:      url,
+		Progress: os.Stdout,
+		// The intended use of a GitHub personal access token is in replace of your password
+		// because access tokens can easily be revoked.
+		// https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
+		Auth: &http.BasicAuth{
+			Username: "abc", // yes, this can be anything except an empty string
+			Password: token,
+		},
+	}
+}
+
+func pushOptions(token string) *git.PushOptions {
+	return &git.PushOptions{
+		Progress: os.Stdout,
+		// The intended use of a GitHub personal access token is in replace of your password
+		// because access tokens can easily be revoked.
+		// https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
+		Auth: &http.BasicAuth{
+			Username: "abc", // yes, this can be anything except an empty string
+			Password: token,
+		},
 	}
 }
 
@@ -39,19 +82,20 @@ func (c *client) repository() (*git.Repository, error) {
 }
 
 func (c *client) cloneOptions() *git.CloneOptions {
-	return &git.CloneOptions{
-		URL:      c.url,
-		Progress: os.Stdout,
-		// The intended use of a GitHub personal access token is in replace of your password
-		// because access tokens can easily be revoked.
-		// https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/
-		Auth: &http.BasicAuth{
-			Username: "abc", // yes, this can be anything except an empty string
-			Password: c.token,
-		},
-	}
+	return cloneOptions(c.url, c.token)
 }
 
-func (c *client) writeFile(filename string, data []byte, perm os.FileMode) error {
-	return util.WriteFile(*c.fs, filename, data, perm)
+func (c *client) pushOptions() *git.PushOptions {
+	return pushOptions(c.token)
+}
+
+func (c *client) commit(message string) (plumbing.Hash, error) {
+	c.commitSignature.When = time.Now()
+	return c.worktree.Commit(message, &git.CommitOptions{
+		Author: c.commitSignature,
+	})
+}
+
+func (c *client) push() error {
+	return c.repo.Push(c.pushOptions())
 }
